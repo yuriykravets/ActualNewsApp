2f958e5b1e0fc79f73d4eecff61c9bff929e8740
Yuriy Kravets <zoidthe311@gmail.com>
2022-07-04 22:57:29 +0300

    Edit details fragment, then settling navigation and working with bundle

--- app/src/main/java/com/yuriycode/actualnewsapp/models/Source.kt
+++ app/src/main/java/com/yuriycode/actualnewsapp/models/Source.kt
@@ -2,5 +2,5 @@ package com.yuriycode.actualnewsapp.models
 
 data class Source(
     val id: String,
-    val name: String
+    val name: String?
 )
--- app/src/main/java/com/yuriycode/actualnewsapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/yuriycode/actualnewsapp/ui/details/DetailsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.yuriycode.actualnewsapp.R
 import com.yuriycode.actualnewsapp.databinding.FragmentDetailsBinding
 
@@ -12,6 +14,7 @@ class DetailsFragment : Fragment() {
 
     private var _binding:FragmentDetailsBinding? = null
     private val mBinding get() = _binding!!
+    private val bundleArgs: DetailsFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater:LayoutInflater, container:ViewGroup?,
@@ -19,5 +22,28 @@ class DetailsFragment : Fragment() {
     ):View? {
         _binding = FragmentDetailsBinding.inflate(layoutInflater, container, false)
         return mBinding.root
+    }
+
+    override fun onViewCreated(view:View, savedInstanceState:Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val articleArg = bundleArgs.article
+
+
+
+        articleArg.let { article ->
+            article.urlToImage.let {
+                Glide.with(this).load(article.urlToImage).into(mBinding.headerImage)
+            }
+            mBinding.headerImage.clipToOutline = true
+            mBinding.articleDetailsTitle.text = article.title
+            mBinding.articleDetailsDescriptionText.text = article.description
+
+            mBinding.articleDetailsButton.setOnClickListener {
+                try {
+
+                } catch ( ex)
+            }
+
+        }
     }
 }
