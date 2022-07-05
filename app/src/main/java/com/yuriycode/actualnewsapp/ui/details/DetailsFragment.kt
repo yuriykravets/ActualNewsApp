25c7cca81ca80e5a9071f71aa306525ad437d2b0
Yuriy Kravets <zoidthe311@gmail.com>
2022-07-05 12:11:20 +0300

    Edit details fragment, then settling navigation and working with bundle

--- app/src/main/java/com/yuriycode/actualnewsapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/yuriycode/actualnewsapp/ui/details/DetailsFragment.kt
@@ -1,10 +1,15 @@
 package com.yuriycode.actualnewsapp.ui.details
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.URLUtil
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.yuriycode.actualnewsapp.R
@@ -40,10 +45,21 @@ class DetailsFragment : Fragment() {
 
             mBinding.articleDetailsButton.setOnClickListener {
                 try {
-
-                } catch ( ex)
+                    Intent()
+                        .setAction(Intent.ACTION_VIEW)
+                        .addCategory(Intent.CATEGORY_BROWSABLE)
+                        .setData(Uri.parse(takeIf { URLUtil.isValidUrl(article.url) }
+                            ?.let {
+                                article.url
+                            } ?: "https://google.com"))
+                        .let {
+                            ContextCompat.startActivity(requireContext(), it, null)
+                        }
+                } catch (e: Exception) {
+                    Toast.makeText(context, "The device doesn't have any browser to view the document!", Toast.LENGTH_SHORT)
+                }
             }
-
         }
     }
+
 }
